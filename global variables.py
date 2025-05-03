7fb048c4c9130fecb4ab2ecd5d704e4413e5f238
Umanga-maker <limbuu063@gmail.com>
2025-05-03 16:15:09 +0545

    Add files via upload

---
+++ global variables.py
@@ -0,0 +1,13 @@
+# print (myfunction())
+
+#global variables
+name = 'TutorialsPoint'
+marks = 50
+result = True
+def myfunction():
+    #accessing inside the function
+    print("name:",name)
+    print("marks:",marks)
+#function call
+myfunction()
+print(globals())
